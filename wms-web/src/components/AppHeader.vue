7a389f32dabf312e579bf03b544c8b2344a2b8d8
AbyssGazer-Oliver <oliverchen43999@gmail.com>
2023-12-18 22:17:11 +0800

    exit login and clear login info

--- wms-web/src/components/AppHeader.vue
+++ wms-web/src/components/AppHeader.vue
@@ -8,7 +8,7 @@
     </div>
     <div class="dropdown">
       <el-dropdown>
-        <span>王小虎</span>
+        <span>{{user.name}}</span>
         <i class="el-icon-arrow-down" style="margin-left: 5px"></i>
         <el-dropdown-menu slot="dropdown">
           <el-dropdown-item @click.native="toUser">个人中心</el-dropdown-item>
@@ -28,6 +28,11 @@
 <script>
 export default {
   name: "AppHeader",
+  data(){
+    return{
+      user: JSON.parse(sessionStorage.getItem('CurUser'))
+    }
+  },
   props:{
     icon:String
   },
@@ -38,6 +43,25 @@ export default {
     },
     logOut(){
       console.log('log out!');
+      this.$confirm('您确定要退出登录吗？', '提示', {
+        confirmButtonText: '确定',  //确认按钮的文字显示
+        type: 'warning',
+        center: true, //文字居中显示
+      })
+          .then(() => {
+            this.$message({
+              type:'success',
+              message: '退出登录成功！'
+            })
+            this.$router.push('/');
+            sessionStorage.clear();
+          })
+          .catch(() => {
+            this.$message({
+              type:'info',
+              message: '已取消退出！'
+            })
+          });
     },
     collapse(){
       this.$emit('doCollapse');
